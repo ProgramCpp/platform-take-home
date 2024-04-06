862e13445c3f6199002b52d26ba4075648ccda37
janani anbarasan <jni@example.com>
2024-04-06 12:57:43 -0500

    provision app by injecting vault secret id

--- contrib/terraform/main.tf
+++ contrib/terraform/main.tf
@@ -38,6 +38,21 @@ data "vault_approle_auth_backend_role_id" "remote_signer" {
   role_name = vault_approle_auth_backend_role.remote_signer.role_name
 }
 
-output "role_id" {
-  value = data.vault_approle_auth_backend_role_id.remote_signer.role_id
+resource "vault_approle_auth_backend_role_secret_id" "remote_signer" {
+  backend   = vault_auth_backend.private_keys.path
+  role_name = vault_approle_auth_backend_role.remote_signer.role_name
+  wrapping_ttl = 60
+}
+
+// TODO: provision the application in a prod environment, with an application provisioner
+// TODO: get vault secret from the application provisioner
+// using local provisioner to simplify sharing vault token with the application, given the application is running locally.
+resource "terraform_data" "launch_remote_signer" {
+  provisioner "local-exec" {
+    command =  "../../build/signer"
+    environment = {
+      ROLE_ID = data.vault_approle_auth_backend_role_id.remote_signer.role_id
+      SECRET_ID = vault_approle_auth_backend_role_secret_id.remote_signer.wrapping_token
+    }
+  }
 }
