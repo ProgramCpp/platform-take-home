5ccf6944f27de7f88fbadacd6e18effa98fb558b
janani anbarasan <jni@example.com>
2024-04-06 21:29:55 -0500

    print vault secrets for tests

--- contrib/terraform/main.tf
+++ contrib/terraform/main.tf
@@ -44,15 +44,11 @@ resource "vault_approle_auth_backend_role_secret_id" "remote_signer" {
   wrapping_ttl = 60
 }
 
-// TODO: provision the application in a prod environment, with an application provisioner
-// TODO: get vault secret from the application provisioner
-// using local provisioner to simplify sharing vault token with the application, given the application is running locally.
-resource "terraform_data" "launch_remote_signer" {
-  provisioner "local-exec" {
-    command =  "../../build/signer"
-    environment = {
-      ROLE_ID = data.vault_approle_auth_backend_role_id.remote_signer.role_id
-      SECRET_ID = vault_approle_auth_backend_role_secret_id.remote_signer.wrapping_token
-    }
-  }
+output "role_id" {
+  value = data.vault_approle_auth_backend_role_id.remote_signer.role_id
+}
+
+// DO NOT DO THIS! this is only for local tests
+output "wrapped_secret_id" {
+  value = nonsensitive(vault_approle_auth_backend_role_secret_id.remote_signer.wrapping_token)
 }
